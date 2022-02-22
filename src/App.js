5fd7417a10d9363674005104a832d73e2a93f75d
rootfellen <rootfellenua@gmail.com>
2022-02-22 10:45:47 -0500

    changes in react router

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from "./components/footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
