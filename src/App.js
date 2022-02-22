cefe45732f6c7bb1cf6c7fd1102db9fe1a267a5c
rootfellen <rootfellenua@gmail.com>
2022-02-22 11:09:37 -0500

    changes in router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Header from "./components/header";
 import Home from "./pages/home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import AddExpense from "./pages/add-expense";
 import Footer from "./components/footer";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/add-expenses" element={<AddExpense />} />
+        <Route path="/add-expenses" element={<AddExpense />} exact />
       </Routes>
       <Footer />
     </Router>
